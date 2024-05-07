bdc22c9ab358143273fa259872b15ba95f4f4a1d
Thu <thibichthu141@gmail.com>
2024-05-07 14:00:14 +0700

    Create test case PP-116

---
+++ step_definitions/men-outerwear/men-outerwear-PP-116.js
@@ -0,0 +1,18 @@
+const { I, menOuterwearPagePP13,menOuterwearPagePP21, menOuterwearPagePP17 } = inject();
+const assert = require('assert');
+  
+Given("I open Men's Outerwear Page", () => {
+      menOuterwearPagePP13.goToWebsite();
+    });
+  //PP-116
+  When("User click X button on the top-right corner of the box", () => {
+    menOuterwearPagePP17.clickOnProduct();
+    menOuterwearPagePP21.clickAddToCartButton();
+    I.click ('#icon');
+  });
+  Then("The dialog box is closed", () => {
+    const classValue = I.grabAttributeFrom('shop-cart-modal','class');
+    assert.notEqual(classValue, "opened");
+  });
+
+
